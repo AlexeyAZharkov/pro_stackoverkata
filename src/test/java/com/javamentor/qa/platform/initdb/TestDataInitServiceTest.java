9b86589bcbec50a8a00d25342a2fb829de4e528b
Лизавета Евдаш <lisenakchek@gmail.com>
2022-10-20 10:03:37 +0000

    Resolve "Реализовать  инициализацию данных"

--- src/test/java/com/javamentor/qa/platform/initdb/TestDataInitServiceTest.java
+++ src/test/java/com/javamentor/qa/platform/initdb/TestDataInitServiceTest.java
@@ -1,33 +1,29 @@
 package com.javamentor.qa.platform.initdb;
 
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.models.entity.question.Tag;
-import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.models.entity.user.reputation.Reputation;
+import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class TestDataInitService {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @Transactional
-    public void createEntity() {
+class TestDataInitServiceTest {
+
+    @Test
+    void createEntity() {
         createUsers();
     }
 
+    @Test
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     List<User> users = new ArrayList<>();
-
 
     private void createUsers() {
         User user = new User();
@@ -48,29 +44,5 @@ public class TestDataInitService {
         user.setNickname("Vladik");
         user.setRole(new Role(1L, "USER"));
         users.add(user);
-    }
-
-    List<Tag> tags = new ArrayList<>();
-
-    private void createTags() {
-
-    }
-
-    List<Question> questions = new ArrayList<>();
-
-    private void createQuestions() {
-
-    }
-
-    List<Answer> answers = new ArrayList<>();
-
-    private void createAnswers() {
-
-    }
-
-    List<Reputation> reputations = new ArrayList<>();
-
-    private void createReputations() {
-
     }
 }
