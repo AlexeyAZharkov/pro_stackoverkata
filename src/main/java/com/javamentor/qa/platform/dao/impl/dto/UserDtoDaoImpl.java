c943162cdc403d957a21feaf08ce870758428782
Лизавета Евдаш <lisenakchek@gmail.com>
2022-10-26 11:37:46 +0000

    Resolve "Разработать api, который возвращает пользователя по его id #17"

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -2,11 +2,13 @@ package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
 import com.javamentor.qa.platform.dao.impl.repository.ReadWriteDaoImpl;
+import com.javamentor.qa.platform.dao.util.SingleResultUtil;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Repository
 public class UserDtoDaoImpl extends ReadWriteDaoImpl<UserDto, Long> implements UserDtoDao {
@@ -14,5 +16,19 @@ public class UserDtoDaoImpl extends ReadWriteDaoImpl<UserDto, Long> implements U
     @PersistenceContext
     private EntityManager entityManager;
 
-
+    @Override
+    public Optional<UserDto> getById(Long id) {
+        return SingleResultUtil.getSingleResultOrNull(entityManager.createQuery("""
+                SELECT new com.javamentor.qa.platform.models.dto.UserDto(
+                u.id,
+                u.email,
+                u.fullName,
+                u.imageLink,
+                u.city,
+                (SELECT coalesce(sum (r.count), 0) from Reputation r where r.author.id = u.id),
+                u.persistDateTime,
+                (SELECT count (distinct va) + count (distinct vq) from VoteAnswer va, VoteQuestion  vq
+                where va.user.id = u.id and vq.user.id = u.id))
+                from User u where u.id=:id""", UserDto.class).setParameter("id", id));
+    }
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
@@ -1,15 +1,31 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
+import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
+import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
+import com.javamentor.qa.platform.models.dto.TagDto;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
 import com.javamentor.qa.platform.service.impl.repository.ReadWriteServiceImpl;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserDtoServiceImpl extends ReadWriteServiceImpl<UserDto, Long> implements UserDtoService {
+import java.util.List;
+import java.util.Optional;
 
-    public UserDtoServiceImpl(ReadWriteDao<UserDto, Long> readWriteDao) {
-        super(readWriteDao);
+@Service
+public class UserDtoServiceImpl implements UserDtoService {
+    private final TagDtoDao tagDtoDao;
+    private final UserDtoDao userDtoDao;
+
+    public UserDtoServiceImpl(TagDtoDao tagDtoDao, UserDtoDao userDtoDao) {
+        this.tagDtoDao = tagDtoDao;
+        this.userDtoDao = userDtoDao;
+    }
+
+    @Override
+    public Optional<UserDto> getById(Long id) {
+        Optional<UserDto> userDto = userDtoDao.getById(id);
+        userDto.ifPresent(dto -> dto.setListTop3TagDto(tagDtoDao.getTop3TagsByUserId(id)));
+        return userDto;
     }
 }
