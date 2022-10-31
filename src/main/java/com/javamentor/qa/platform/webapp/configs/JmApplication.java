fac6c4e3ba5e930e3ff7939ab83c4f58139aa0cf
iTim555 <timur555@yandex.ru>
2022-10-31 17:22:27 +0300

    refactor

--- src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
@@ -1,29 +1,18 @@
 package com.javamentor.qa.platform.webapp.configs;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @ComponentScan("com.javamentor.qa.platform")
 @EntityScan("com.javamentor.qa.platform.models.entity")
-public class JmApplication extends SpringBootServletInitializer {
+public class JmApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(JmApplication.class, args);
     }
+
 }
+
