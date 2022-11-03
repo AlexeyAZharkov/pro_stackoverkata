e475abf6c35a3cc326b01789c1a16f4be0305864
Алексей Жарков <azharkov@mail.ru>
2022-11-03 08:41:01 +0000

    Resolve "Реализовать api проголосовать за Question, голосование за вопрос #26"

--- src/main/java/com/javamentor/qa/platform/service/impl/model/ReputationServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/ReputationServiceImpl.java
@@ -2,12 +2,14 @@ package com.javamentor.qa.platform.service.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.ReputationDao;
 import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.models.entity.user.reputation.Reputation;
+import com.javamentor.qa.platform.models.entity.user.reputation.ReputationType;
 import com.javamentor.qa.platform.service.abstracts.model.ReputationService;
 import com.javamentor.qa.platform.service.impl.repository.ReadWriteServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
 
 @Service
@@ -19,9 +21,17 @@ public class ReputationServiceImpl extends ReadWriteServiceImpl<Reputation, Long
         super(readWriteDao);
         this.reputationDao = reputationDao;
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Reputation> getReputation(User user, Question question, ReputationType reputationType) {
+        return reputationDao.getReputation(user, question, reputationType);
+    }
+
     @Override
     @Transactional
     public Optional<Reputation> getByAnswerIdAndSenderId(Long answerId, Long userId) {
         return reputationDao.getByAnswerIdAndSenderId(answerId, userId);
     }
+
 }
