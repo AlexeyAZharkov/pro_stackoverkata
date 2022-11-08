df9292ca6cecf6fff31881fe1f4a62b9af3d77d5
iTim555 <timur555@yandex.ru>
2022-11-08 12:11:48 +0300

    refactor

--- src/main/java/com/javamentor/qa/platform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/javamentor/qa/platform/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.javamentor.qa.platform.controller;
+package com.javamentor.qa.platform.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
