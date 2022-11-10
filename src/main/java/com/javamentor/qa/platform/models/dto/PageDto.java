bb85b9d7c2375a3d662d8086daae0ab99d834610
Мария Короткевич <maria.korotkevich@yandex.ru>
2022-11-10 15:03:28 +0000

    Resolve "Разработать универсальную пагинацию для всех Dto"

---
+++ src/main/java/com/javamentor/qa/platform/models/dto/PageDto.java
@@ -0,0 +1,20 @@
+package com.javamentor.qa.platform.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageDto<T> {
+    private int currentPageNumber;
+    private int totalPageCount;
+    private int totalResultCount;
+    private List<T> items;
+    private int itemsOnPage;
+}
