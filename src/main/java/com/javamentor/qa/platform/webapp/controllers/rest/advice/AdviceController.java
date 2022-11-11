be7b5b431120872d7f15a95b1cdc8b590cc5db78
Алексей Жарков <azharkov@mail.ru>
2022-11-11 14:17:38 +0000

    Resolve "Провести рефактор AdviceController"

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/advice/AdviceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/advice/AdviceController.java
@@ -1,15 +1,35 @@
 package com.javamentor.qa.platform.webapp.controllers.rest.advice;
 
+import com.javamentor.qa.platform.exception.AnswerException;
+import com.javamentor.qa.platform.exception.ApiRequestException;
+import com.javamentor.qa.platform.exception.ConstrainException;
+import com.javamentor.qa.platform.exception.VoteException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class AdviceController {
+public class AdviceController extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handlerException(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(AnswerException.class)
+    public ResponseEntity<Object> handlerAnswerException(AnswerException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ApiRequestException.class)
+    public ResponseEntity<Object> handlerApiRequestException(ApiRequestException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstrainException.class)
+    public ResponseEntity<Object> handlerConstrainException(ConstrainException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(VoteException.class)
+    public ResponseEntity<Object> handlerVoteException(VoteException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
